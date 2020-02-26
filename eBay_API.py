558bd1ea67f4f2656200c299a0522dc43831fd9d
Jarwy <jarwy880413@gmail.com>
2020-02-26 17:35:53 +0800

    20200226

--- eBay_API.py
+++ eBay_API.py
@@ -7,94 +7,126 @@ Created on Thu Feb 13 04:35:01 2020
 
 import time;
 import pymongo;
-import pandas as pd;
+import datetime;
 from bs4 import BeautifulSoup;
 from ebaysdk.finding import Connection as finding;
 
 
-
-
-
 Keywords = "ssd 128gb";
 entriesPerPage = 25
+flag = 0
 api = finding(appid ='ChiaWeiH-20200213-PRD-fca7fae46-eb3bd471'
-#              ,domain='svcs.sandbox.ebay.com'
               ,debug=True
               ,config_file= None
              );
               
-
-
 api_request = {"keywords":Keywords
                ,"outputSelector":"SellerInfo"
                , 'paginationInput': {'entriesPerPage': entriesPerPage,'pageNumber': 1 }
               };
-  
+# Fetch time
+now = datetime.datetime.now()
 
+sched_timer = datetime.datetime(now.year, now.month, now.day, now.hour, now.minute,
+                                now.second) + datetime.timedelta(seconds=5)
 
+# sched_timer = datetime.datetime(2017,12,13,9,30,10)
+while (True):
 
-#response = api.execute('findItemsAdvanced', api_request)
-response = api.execute('findCompletedItems', api_request);
-time.sleep(5);
-soup = BeautifulSoup(response.content, "lxml");
+    now = datetime.datetime.now()
 
-totalentries = int(soup.find("totalentries").text);
-items = soup.find_all("item");
+    if sched_timer < now < sched_timer + datetime.timedelta(seconds=1):
+        time.sleep(1)
+        
+        
+        # 主程序
+        # =============================================================================
+        # <Ebay API>
+        # =============================================================================
+       
+        response = api.execute('findItemsByKeywords', api_request);
+        soup = BeautifulSoup(response.content, "lxml");
+        
+        totalentries = int(soup.find("totalentries").text);
+        items = soup.find_all("item",limit = None);
+        
+        #input(items[0]);
+        
+        # =============================================================================
+        # <Construct the findings>
+        # =============================================================================
+        #IDList = []
+        IDList = [];
+        CatList =[];
+        TitleList = [];
+        PriceList = [];
+        SiteList = [];
+        SellerList = [];
+        
+        for item in items:
+            itemid = item.itemid.string.lower();
+            IDList.append(itemid);
+            cat = item.categoryname.string.lower();
+            CatList.append(cat);
+            title = item.title.string.lower().strip();
+            TitleList.append(title);
+            price = float(item.currentprice.string);
+            PriceList.append(price);
+            site = item.viewitemurl.string.lower();
+            SiteList.append(site);
+            seller = item.sellerusername.string.lower();
+            SellerList.append(seller);
+        
+        X = totalentries;
+        if(X >=100):
+            boundery = 100;
+        else:
+            boundery = totalentries;
+        
+        # 
+        #for i in range(0,boundery):
+        #    print("____________________________________________________________________________________________________________________________");
+        #    print(ItemList[i],"\n");
+        #    print(CatList[i],"\n");
+        #    print(TitleList[i],"\n");
+        #    print(PriceList[i],"\n");
+        #    print(SellerList[i],"\n");
+        #    print(SiteList[i],"\n");
+            
+        
+        
+        
+        # =============================================================================
+        # <Connect with MongoDB>
+        # =============================================================================
+        myclient = pymongo.MongoClient("mongodb://localhost:27017/");
+        mydb = myclient["Ebay"];
+        mycol = mydb[Keywords];
 
-#input(items[0]);
-DateList = [];
-IDList = [];
-CatList =[];
-TitleList = [];
-PriceList = [];
-SiteList = [];
-SellerList = [];
-#ShippingInfoList = [];
-
-
-for item in items:
-    date = item.starttime.string.lower();
-    DateList.append(date);
-    itemid = item.itemid.string.lower();
-    IDList.append(itemid);
-    cat = item.categoryname.string.lower();
-    CatList.append(cat);
-    title = item.title.string.lower().strip();
-    TitleList.append(title);
-    price = float(item.currentprice.string);
-    PriceList.append(price);
-    site = item.viewitemurl.string.lower();
-    SiteList.append(site);
-    seller = item.sellerusername.string.lower();
-    SellerList.append(seller);
-
-# 
-for i in range(0,4):
-    print("____________________________________________________________________________________________________________________________");
-    print(DateList[i],"\n");
-    print(IDList[i],"\n");
-    print(CatList[i],"\n");
-    print(TitleList[i],"\n");
-    print(PriceList[i],"\n");
-    print(SellerList[i],"\n");
-    print(SiteList[i],"\n");
+        for i in range(0,entriesPerPage-1):
     
+#           print(CatList[i],"\n");
+#           print(TitleList[i],"\n");
+#           print(PriceList[i],"\n");
+#           print(SellerList[i],"\n");
+#           print(SiteList[i],"\n"); 
+            INFO = { "Category": CatList[i], "Product Name": TitleList[i], "Price": PriceList[i], "Seller": SellerList[i], "Site": SiteList[i]}
+            x = mycol.insert_one(INFO) 
+            print(x)
+        # =============================================================================
+        # <Build Dataframe>
+        # =============================================================================
+        #FullList = [IDList,CatList,TitleList,PriceList,SellerList, SiteList]   
+        #df = pd.DataFrame(zip(*FullList), columns=['ID','Catrgory', 'Item Name', 'Price', 'Seller', 'Site']); 
+                
+        
+        
+        flag = 1
+    else:
+        
+        if flag == 1:
+           
+            sched_timer = sched_timer + datetime.timedelta(seconds=30)
+            flag = 0
 
-FullList = [DateList,IDList,CatList,TitleList,PriceList,SellerList, SiteList];
-    
-df = pd.DataFrame(zip(*FullList), columns=['Date','ID','Catrgory', 'Item Name', 'Price', 'Seller', 'Site']);  
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017/");
-mydb = myclient["Ebay"];
-mycol = mydb[Keywords];
-
-for i in range(0,entriesPerPage-1):
-    
-#    print(CatList[i],"\n");
-#    print(TitleList[i],"\n");
-#    print(PriceList[i],"\n");
-#    print(SellerList[i],"\n");
-#    print(SiteList[i],"\n"); 
-    INFO = { "Category": CatList[i], "Product Name": TitleList[i], "Price": PriceList[i], "Seller": SellerList[i], "Site": SiteList[i]}
-    x = mycol.insert_one(INFO) 
-    print(x)
